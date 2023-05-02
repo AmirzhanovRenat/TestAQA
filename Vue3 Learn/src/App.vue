0743ee73651735963b674b35963319efa48a2829
Ренат Амиржанов <amirzhanovrt@63pokupki.ru>
2023-05-02 16:27:54 +0400

    vue

---
+++ Vue3 Learn/src/App.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+      text: 'hello',
+    };
+  },
+  methods: {
+    func(par) {
+      this.text = par;
+    },
+    addPar() {
+      this.text = 'UUUU';
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <p>{{ text }}</p>
+  </div>
+
+  <button v-on:click="func('miruMir')">ClickHere</button>
+
+  <button @click="addPar">Click88</button>
+  <div>
+    <a :href="text">text</a>
+  </div>
+</template>
+
+<style scoped></style>
